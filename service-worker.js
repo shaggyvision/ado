b616fbc92d200ed35b179fbada9de946e510791a
shaggyvision <137138189+shaggyvision@users.noreply.github.com>
2025-10-26 11:36:16 -0600

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-
-self.addEventListener('install', function(e) {
-    console.log('Service Worker instalado');
-});
-self.addEventListener('fetch', function(e) {
-    console.log('Interceptando petición: ', e.request.url);
-});
